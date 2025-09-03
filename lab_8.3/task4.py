7ddc9c9810abd3a726c648f0faa1368ea7c88fd6
Bhaskarreddy008 <2403a51008@sru.edu.in>
2025-09-03 21:30:28 +0530

    Add files via upload

---
+++ lab_8.3/task4.py
@@ -0,0 +1,13 @@
+class ShoppingCart:
+    def _init_(self):
+        self.items = {}
+
+    def add_item(self, name, price):
+        self.items[name] = price
+
+    def remove_item(self, name):
+        if name in self.items:
+            del self.items[name]
+
+    def total_cost(self):
+        return sum(self.items.values())
